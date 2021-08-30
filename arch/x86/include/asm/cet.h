1c541fb83944341cc3fe61a7932b7fd8a10a4285
Yu-cheng Yu <yu-cheng.yu@intel.com>
2021-08-30 11:15:21 -0700

    x86/shstk: Handle thread shadow stack
    When a process is duplicated, but the child shares the address space with
    the parent, there is potential for the threads sharing a single stack to
    cause conflicts for each other. In the normal non-cet case this is handled
    in two ways.
    
    With regular CLONE_VM a new stack is provided by userspace such that the
    parent and child have different stacks.
    
    For vfork, the parent is suspended until the child exits. So as long as
    the child doesn't return from the vfork()/CLONE_VFORK calling function and
    sticks to a limited set of operations, the parent and child can share the
    same stack.
    
    For shadow stack, these scenarios present similar sharing problems. For the
    CLONE_VM case, the child and the parent must have separate shadow stacks.
    Instead of changing clone to take a shadow stack, have the kernel just
    allocate one and switch to it.
    
    Use stack_size passed from clone3() syscall for thread shadow stack size. A
    compat-mode thread shadow stack size is further reduced to 1/4. This
    allows more threads to run in a 32-bit address space. The clone() does not
    pass stack_size, which was added to clone3(). In that case, use
    RLIMIT_STACK size and cap to 4 GB.
    
    For shadow stack enabled vfork(), the parent and child can share the same
    shadow stack, like they can share a normal stack. Since the parent is
    suspended until the child terminates, the child will not interfere with
    the parent while executing as long as it doesn't return from the vfork()
    and overwrite up the shadow stack. The child can safely overwrite down
    the shadow stack, as the parent can just overwrite this later. So CET does
    not add any additional limitations for vfork().
    
    Userspace implementing posix vfork() can actually prevent the child from
    returning from the vfork() calling function, using CET. Glibc does this
    by adjusting the shadow stack pointer in the child, so that the child
    receives a #CP if it tries to return from vfork() calling function.
    
    Free the shadow stack on thread exit by doing it in mm_release(). Skip
    this when exiting a vfork() child since the stack is shared in the
    parent.
    
    During this operation, the shadow stack pointer of the new thread needs
    to be updated to point to the newly allocated shadow stack. Since the
    ability to do this is confined to the FPU subsystem, change
    fpu_clone() to take the new shadow stack pointer, and update it
    internally inside the FPU subsystem. This part was suggested by Thomas
    Gleixner.
    
    Suggested-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Yu-cheng Yu <yu-cheng.yu@intel.com>
    Co-developed-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    
    v3:
     - Fix update_fpu_shstk() stub (Mike Rapoport)
     - Fix chunks around alloc_shstk() in wrong patch (Kees)
     - Fix stack_size/flags swap (Kees)
     - Use centalized stack size logic (Kees)
    
    v2:
     - Have fpu_clone() take new shadow stack pointer and update SSP in
       xsave buffer for new task. (tglx)
    
    v1:
     - Expand commit log.
     - Add more comments.
     - Switch to xsave helpers.
    
    Yu-cheng v30:
     - Update comments about clone()/clone3(). (Borislav Petkov)
    
    Yu-cheng v29:
     - WARN_ON_ONCE() when get_xsave_addr() returns NULL, and update comments.
       (Dave Hansen)
    
    Yu-cheng v28:
     - Split out copy_thread() argument name changes to a new patch.
     - Add compatibility for earlier clone(), which does not pass stack_size.
     - Add comment for get_xsave_addr(), explain the handling of null return
       value.

--- arch/x86/include/asm/cet.h
+++ arch/x86/include/asm/cet.h
@@ -15,11 +15,18 @@ struct thread_shstk {
 
 long cet_prctl(struct task_struct *task, int option, unsigned long features);
 void reset_thread_features(void);
+int shstk_alloc_thread_stack(struct task_struct *p, unsigned long clone_flags,
+			     unsigned long stack_size,
+			     unsigned long *shstk_addr);
 void shstk_free(struct task_struct *p);
 #else
 static inline long cet_prctl(struct task_struct *task, int option,
 			     unsigned long features) { return -EINVAL; }
 static inline void reset_thread_features(void) {}
+static inline int shstk_alloc_thread_stack(struct task_struct *p,
+					   unsigned long clone_flags,
+					   unsigned long stack_size,
+					   unsigned long *shstk_addr) { return 0; }
 static inline void shstk_free(struct task_struct *p) {}
 #endif /* CONFIG_X86_USER_SHADOW_STACK */
 
